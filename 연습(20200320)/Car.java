35f270c9d069d06e45edc55f7d270d037d963dd1
tmddnr712 <53157978+tmddnr712@users.noreply.github.com>
2020-04-13 18:52:41 +0900

    Add files via upload

---
+++ 연습(20200320)/Car.java
@@ -0,0 +1,26 @@
+
+/**
+ * 자동차 클래스
+ * 
+ * @author (2018210083) 
+ * @version (20200320)
+ */
+public class Car
+{
+    // 인스턴스 변수 - 다음의 예제를 사용자에 맞게 바꾸십시오
+    private int engine;
+    private int number;
+    private String company;
+    
+    /**
+     * 예제 메소드 - 이 주석을 사용자에 맞게 바꾸십시오
+     * 
+     * @param  y   메소드의 예제 매개변수
+     * @return     x 더하기 y 
+     */
+    public void Drive()
+    {
+        // 여기에 코드를 작성하십시오.
+        System.out.println("Hi~~");
+    }
+}
