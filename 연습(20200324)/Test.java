35039cf3d1b57a5581b4d193b1bd3ba7b4f76854
tmddnr712 <53157978+tmddnr712@users.noreply.github.com>
2020-04-13 18:53:14 +0900

    Add files via upload

---
+++ 연습(20200324)/Test.java
@@ -0,0 +1,14 @@
+
+/**
+ * 첫 자바 프로그램(학번과 이름을 출력)
+ * 
+ * @author (2018210083 노승욱)
+ * @version (2020.03.24)
+ */
+public class Test
+{
+    public static void main(String[] args)
+    {
+        System.out.println("2018210083 노승욱");
+    }
+}
