ddf13f88b3fb3b4b9bd8a1d85f84090c511121ad
tmddnr712 <53157978+tmddnr712@users.noreply.github.com>
2020-06-13 19:41:09 +0900

    Add files via upload

---
+++ 실습#3/실습#3/Mul.java
@@ -0,0 +1,21 @@
+
+/**
+ * 실습#3
+ * 
+ * @author (2018210083_노승욱) 
+ * @version (2020.06.12)
+ */
+public class Mul
+{
+    public int a;
+    public int b;
+    
+    public int calculate(){
+        return this.a * this.b;
+    }
+    
+    public void setValue(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+}
---
+++ 실습#3/실습#3/MyApp.java
@@ -0,0 +1,13 @@
+
+/**
+ * 실습#3
+ * 
+ * @author (2018210083_노승욱) 
+ * @version (2020.06.12)
+ */
+public class MyApp
+{
+    public static void main(String[] args){
+        System.out.println("두 정수와 연산자를 입력하시오");
+    }
+}
